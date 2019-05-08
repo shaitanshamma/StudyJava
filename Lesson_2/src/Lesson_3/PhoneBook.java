09564b4b81f0c8a73abff764787b26adfa163aa0
shaitanshamma <kurbatov_andr@hotmail.com>
2019-05-09 00:39:04 +0300

    Test_1

---
+++ Lesson_2/src/Lesson_3/PhoneBook.java
@@ -0,0 +1,12 @@
+package Lesson_3;
+
+public class PhoneBook {
+    String lastName;
+    int phone;
+    public PhoneBook(String lastName, int phone) {
+        this.lastName = lastName;
+        this.phone = phone;
+    }
+
+
+}
