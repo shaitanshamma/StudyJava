8bcf5ab50d856f6d060aa81e131aa4b8adb864ed
shaitanshamma <kurbatov_andr@hotmail.com>
2019-05-11 19:23:08 +0300

    Первое приближение

--- Lesson_2/src/Lesson_3/PhoneBook.java
+++ Lesson_2/src/Lesson_3/PhoneBook.java
@@ -1,12 +1,40 @@
 package Lesson_3;
 
+import java.util.*;
+
 public class PhoneBook {
     String lastName;
+
+    public int getPhone(String lastName) {
+        return phone;
+    }
+
     int phone;
+
+    public String getLastName() {
+        return lastName;
+    }
+
     public PhoneBook(String lastName, int phone) {
         this.lastName = lastName;
         this.phone = phone;
     }
 
+    public String toString() {
+        return lastName + ":" + phone;
+    }
 
+    public static void main(String[] args) {
+        LinkedList<PhoneBook> phoneBook = new LinkedList<>();
+        phoneBook.add(new PhoneBook("Вася", 1112345));
+        phoneBook.add(new PhoneBook("Вася", 1233345));
+        phoneBook.add(new PhoneBook("Аня", 1232245));
+        phoneBook.add(new PhoneBook("Аня", 12121345));
+        for (PhoneBook phone : phoneBook) {
+            System.out.println(phone.getPhone("Вася"));
+        }
+     }
 }
+
+
+
