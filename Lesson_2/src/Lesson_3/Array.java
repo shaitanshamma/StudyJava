8daa4911b1d96a0f6d11a5a05c640e427dd5a870
shaitanshamma <kurbatov_andr@hotmail.com>
2019-05-11 19:22:32 +0300

    Убрал дублирование

--- Lesson_2/src/Lesson_3/Array.java
+++ Lesson_2/src/Lesson_3/Array.java
@@ -3,6 +3,7 @@ package Lesson_3;
 import java.util.*;
 
 public class Array {
+
     public static void main(String[] args) {
         LinkedList<String> arrayList = new LinkedList<>();
         arrayList.add("дом");
@@ -22,28 +23,34 @@ public class Array {
         arrayList.add("отпуск");
         arrayList.add("отпуск");
 
-        Iterator<String> itr = arrayList.iterator();
+        //      Iterator<String> itr = arrayList.iterator();
         Set<String> set = new HashSet<>(arrayList);
 
         System.out.println(set);
         System.out.println(arrayList);
-        while (itr.hasNext()) {
+//        while (itr.hasNext()) {
+//
+//            int num = 0;
+//            String res = itr.next();
+//            for (int i = 0; i < arrayList.size(); i++) {
+//                if (res == arrayList.get(i)) {
+//                    num++;
+//
+//                }
+//            }
+//            if (num == 1) {
+//                System.out.println("слово " + res + " встречается один раз");
+//            } else {
+//                System.out.println("слово " + res + " встречается " + num + " раза");
+//
+//            }
+//        }
 
-            int num = 0;
-            String res = itr.next();
-            for (int i = 0; i < arrayList.size(); i++) {
-                if (res == arrayList.get(i)) {
-                    num++;
-
-                }
-            }
-            if (num == 1) {
-                System.out.println("слово " + res+ " встречается один раз");
-            }else {
-                System.out.println("слово " + res + " встречается " + num +" раза");
-
-            }
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String str : arrayList) {
+            hm.put(str, hm.getOrDefault(str, 0) + 1);
         }
-
+        System.out.println(hm);
     }
+
 }
