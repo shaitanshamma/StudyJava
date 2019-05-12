993c7c8ab9575ce8c961961d2a9c847e8a700c3f
shaitanshamma <kurbatov_andr@hotmail.com>
2019-05-12 11:05:06 +0300

    Новая телефонная книга на Мапе

--- Lesson_2/src/Lesson_3/PhoneBook.java
+++ Lesson_2/src/Lesson_3/PhoneBook.java
@@ -3,37 +3,42 @@ package Lesson_3;
 import java.util.*;
 
 public class PhoneBook {
-    String lastName;
 
-    public int getPhone(String lastName) {
-        return phone;
+    HashMap<String, HashSet<String>> contact = new HashMap<>();
+    HashSet<String> phone = new HashSet<>();
+
+    public void add(String lastName, String phoneNumber) {
+        if (contact.containsKey(lastName)) {
+            phone = contact.get(lastName);
+            phone.add(phoneNumber);
+            contact.put(lastName, phone);
+        } else {
+            phone = new HashSet<>();
+            phone.add(phoneNumber);
+            contact.put(lastName, phone);
+        }
     }
 
-    int phone;
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public PhoneBook(String lastName, int phone) {
-        this.lastName = lastName;
-        this.phone = phone;
-    }
-
-    public String toString() {
-        return lastName + ":" + phone;
+    public void getPhone(String lastName) {
+        System.out.println("По имени " + lastName + " найдено");
+        System.out.println(contact.get(lastName));
     }
 
     public static void main(String[] args) {
-        LinkedList<PhoneBook> phoneBook = new LinkedList<>();
-        phoneBook.add(new PhoneBook("Вася", 1112345));
-        phoneBook.add(new PhoneBook("Вася", 1233345));
-        phoneBook.add(new PhoneBook("Аня", 1232245));
-        phoneBook.add(new PhoneBook("Аня", 12121345));
-        for (PhoneBook phone : phoneBook) {
-            System.out.println(phone.getPhone("Вася"));
-        }
-     }
+        PhoneBook phoneBook1 = new PhoneBook();
+        phoneBook1.add("Вася", "1236");
+        phoneBook1.add("Вася", "112244");
+        phoneBook1.add("Вася", "11212244");
+        phoneBook1.add("Аня", "12244");
+        phoneBook1.add("Вася", "145612244");
+        phoneBook1.add("Аня", "11227744");
+        phoneBook1.add("Вася", "112244");
+        phoneBook1.add("Петя", "11244244");
+
+        phoneBook1.getPhone("Вася");
+        phoneBook1.getPhone("Аня");
+    }
+
 }
 
 
