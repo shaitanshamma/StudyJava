f3dc19ec17cfb11eb766991e2fcc5de3753e24f5
shaitanshamma <kurbatov_andr@hotmail.com>
2019-05-18 10:10:59 +0300

    Наброски чата

---
+++ Chatic/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+
+
+    public void sendMsg() {
+        textArea.appendText("Вася : " + textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
