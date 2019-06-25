ca6b017cbb328c9e56fc57eb4d418521920356fa
shaitanshamma <kurbatov_andr@hotmail.com>
2019-06-25 20:48:38 +0300

    Запись логов на сервер

--- Lesson_6/src/Server/StartChat.java
+++ Lesson_6/src/Server/StartChat.java
@@ -1,9 +1,14 @@
 package Server;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class StartChat {
     public static void main(String[] args) throws SQLException {
-        new ChatServer();
+        try {
+            new ChatServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
